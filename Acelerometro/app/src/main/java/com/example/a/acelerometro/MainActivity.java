cad6b5c7867a8f19415efb85bc997ecb9d2fe6df
NoseCrater2 <s14120039@alumnos.itsur.edu.mx>
2018-09-17 15:32:43 -0500

    Proximidad y Acelerometros
    Se agregaron los ejemplos del uso de los sensores de proximidad y acelerometro

---
+++ Acelerometro/app/src/main/java/com/example/a/acelerometro/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.a.acelerometro;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    int whip=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensor==null){
+            finish();
+        }
+
+        sensorEventListener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x=event.values[0];
+                if(x<-5 && whip==0){
+                    whip++;
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                }else if(x>5 && whip==1){
+                    whip++;
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                }
+
+                if(whip==2){
+                    sound();
+                    whip=0;
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        start();
+    }
+    private void sound(){
+        MediaPlayer mediaPlayer=MediaPlayer.create(this,R.raw.latigo);
+        mediaPlayer.start();
+    }
+
+    public void start(){
+        sensorManager.registerListener(sensorEventListener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    public void stop(){
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    @Override
+    protected void onPause() {
+        stop();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        start();
+        super.onResume();
+    }
+}
